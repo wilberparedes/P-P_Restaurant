0181e8e4aa37e8ba2b2f06bff8fc6c7b24f93fd3
Wilber Paredes <wilber.paredes@tecnicall.com.co>
2023-03-30 10:52:58 -0500

    chore: install react hook form and config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#FF5C26',
+          500: '#FF5C26',
+        },
+        info: {
+          DEFAULT: '#48C1F2',
+          600: '#3682F4',
+          500: '#48C1FE',
+        },
+        text: {
+          DEFAULT: '#4A4A4A',
+          500: '#4A4A4A',
+          400: '#5F5F5F',
+        },
+        success: {
+          DEFAULT: '#47C257',
+          500: '#47C257',
+        },
+        disabled: {
+          DEFAULT: '#C8C8C8',
+          500: '#C8C8C8',
+        },
+        gray: {
+          DEFAULT: '#979797',
+          50: '#F4F4F4',
+        },
+      },
+      fontFamily: {
+        icomoon: ['icomoon'],
+      },
+      backgroundImage: {
+        login: "url('/public/images/login-pide-y-pasa-image.png')",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
