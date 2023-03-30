530d097a52c7023bcba4266d7bed08684c5bea63
Wilber Paredes <wilber.paredes@tecnicall.com.co>
2023-03-30 11:30:03 -0500

    chore: add contants countries

---
+++ src/modules/common/constants/countries/index.ts
@@ -0,0 +1,21 @@
+export const COUNTRIES: {
+  code: string
+  flag: string
+  phoneCode: string
+}[] = [
+  {
+    code: 'CO',
+    flag: '🇨🇴',
+    phoneCode: '57',
+  },
+  {
+    code: 'AR',
+    flag: '🇦🇷',
+    phoneCode: '54',
+  },
+  {
+    code: 'MX',
+    flag: '🇲🇽',
+    phoneCode: '52',
+  },
+]
