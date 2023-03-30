797d5f8582f75295535a0364fa703cb282307246
Wilber Paredes <wilber.paredes@tecnicall.com.co>
2023-03-30 04:13:44 -0500

    chore: config font monserrat

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,9 +6,54 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     outline:0;
     box-sizing:border-box;
-    font-family: 'Open Sans', sans-serif;
+    font-family: 'Montserrat', sans-serif;
   }
   #root{
       margin:0 auto;
   }
+  html, body, #root{
+    height: 100%;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  @-webkit-keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  .fadeIn {
+    -webkit-animation-duration: 0.5s;
+    animation-duration: 0.5s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+
+    -webkit-animation-name: fadeIn;
+    animation-name: fadeIn;
+  }
+
+  select {
+    font-family: inherit;
+    /** for the dropdown indicator */
+    appearance: none;
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-position: right 10px center;
+    background-size: 1em;
+}
+
+input[type="date"]::-webkit-calendar-picker-indicator {
+    background: none
+}
 `
