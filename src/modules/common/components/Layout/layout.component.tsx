4d35f894ce2ba37faf326e84e4286009ab73dc02
Wilber Paredes <wilber.paredes@tecnicall.com.co>
2023-03-30 11:20:00 -0500

    feat: component layout

--- src/modules/common/components/Layout/layout.component.tsx
+++ src/modules/common/components/Layout/layout.component.tsx
@@ -1,10 +1,42 @@
 import { FC, PropsWithChildren } from 'react'
 
-export const Layout: FC<PropsWithChildren> = ({ children }) => {
+import { useNavigate } from 'react-router-dom'
+
+import { NavBar } from '@/modules/common/components'
+
+import { BackFixed, Body, ContainerBack, LinkSyled } from './layout.style'
+import { LayoutProps } from './layout.props'
+
+export const Layout: FC<PropsWithChildren<LayoutProps>> = ({
+  children,
+  goBack = false,
+}) => {
+  const navigate = useNavigate()
+
   return (
     <>
-      <h1>Hola Soy Layout</h1>
-      {children}
+      <NavBar />
+
+      {goBack ? (
+        <ContainerBack>
+          <BackFixed>
+            <LinkSyled
+              to={'..'}
+              onClick={(e) => {
+                e.preventDefault()
+                navigate(-1)
+              }}
+            >
+              <i className='icon-back' />
+              Anterior
+            </LinkSyled>
+          </BackFixed>
+        </ContainerBack>
+      ) : (
+        <div className='h-8' />
+      )}
+
+      <Body>{children}</Body>
     </>
   )
 }
--- src/modules/common/components/Layout/layout.style.ts
+++ src/modules/common/components/Layout/layout.style.ts
@@ -1,5 +1,29 @@
 import tw from 'tailwind-styled-components'
+import { Link } from 'react-router-dom'
 
-export const Container = tw.div<any>`
-  
+export const Body = tw.div<any>`
+pt-14
+px-6
+pb-6
 `
+
+export const ContainerBack = tw.div<any>`
+pt-16
+`
+
+export const BackFixed = tw.div<any>`
+fixed
+bg-white
+w-full
+px-6
+py-4
+flex
+justify-start`
+
+export const LinkSyled = tw(Link)`
+flex
+font-bold
+text-sm
+text-info-600
+items-center
+justify-center`
