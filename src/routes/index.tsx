ea4ce5cc7637bb2a54f6d6baa676b57ccc26d9ce
Wilber Paredes <wilber.paredes@tecnicall.com.co>
2023-03-30 11:59:26 -0500

    feat: add routes for register

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,25 @@ import { lazy, Suspense } from 'react'
 
 import { BrowserRouter, Routes as RoutesDom, Route } from 'react-router-dom'
 
-import { PreloaderComponents } from '@/modules/common/components'
+import { Register } from '@/modules/register'
+import { CellPhone, Password, Profile } from '@/modules/register/components'
 
 const DashboardPage = lazy(() => import('@/pages/dashboard'))
 const LoginPage = lazy(() => import('@/pages/login'))
+const RegisterPage = lazy(() => import('@/pages/register'))
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<PreloaderComponents />}>
+      <Suspense fallback={null}>
         <RoutesDom>
           <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />}>
+            <Route path='/register' element={<Register />} />
+            <Route path='cellphone' element={<CellPhone />} />
+            <Route path='password' element={<Password />} />
+            <Route path='profile' element={<Profile />} />
+          </Route>
           <Route path='/' element={<DashboardPage />} />
           <Route
             path='*'
